281527a17f72fa39fefdfe42f1f0367f243361a1
Dinatang <dm.tanguilav@uea.edu.ec>
2026-03-27 17:49:31 -0500

    subir

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        // Required by some plugins (e.g. flutter_local_notifications) that use Java 8+ APIs.
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,4 +43,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
